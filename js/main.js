513dc913d3bf861620cd4f6a502876a1443f4dfd
unknown <trigition@gmail.com>
2015-04-21 13:24:36 -0600

    Almost there, graphical issues remain

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 var width = 960,
     height = 480;
-var month = ["01","02","03","04","05","06","07","08","09","10","11","12"],
+var months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"],
     columns = [],
+	orderedColumns = [],
     curFrame = 0,
     interval,
     frameLength = 500,
@@ -23,12 +24,14 @@ var coordinates = [];
 			
 d3.json("us-10m.json", function(error, us) {
     d3.csv("data.csv", function(error, data) {
+		//orderedColumns.push(data.TIME_INDEX);
+		//console.log(orderedColumns);
         g.selectAll("circle")
             .data(data)
             .enter()
             .append("circle")
             .attr("cx", function(d) {
-                //console.log(d)
+                orderedColumns.push(d.TIME_INDEX)
                 var coor = projection([d.BEGIN_LON, d.BEGIN_LAT])
                 if (coor !== null)
                     return coor[0];
@@ -49,40 +52,119 @@ d3.json("us-10m.json", function(error, us) {
                 //console.log(magnitude)
                 return magnitude;
             })
-            //.style("fill", "red")
-            .style("opacity", 0.5);
+            .attr("time_index", function(d) {
+				return d.TIME_INDEX;	
+			})
+            .style("opacity", 0);
+		//Generate sliders	
+		//console.log(orderedColumns);
+		dateScale = createDataScale(orderedColumns);
+		generateSlider();
     });
+	//Draw Map Components
     g.insert("path", ".graticule")
         .datum(topojson.feature(us, us.objects.land))
         .attr("class", "land")
         .attr("d", path);
-    /*g.insert("path", ".graticule")
-        .datum(topojson.mesh(us, us.objects.counties, function(a, b) { return a !== b && !(a.id / 1000 ^ b.id / 1000); }))
-        .attr("class", "county-boundary")
-        .attr("d", path);*/
-    g.insert("path", ".graticule")
+    /*g.append("path")
         .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
         .attr("class", "state-boundary")
-            //.attr("vector-effect", "non-scaling-stroke")
-        .attr("d", path);
+        .attr("d", path);*/
+	//Generate sliders
+
 });
+
+/*
+ * Animation definition for circles
+ */
+function drawGlyphs(index) {
+	console.log(index)
+	var circle = d3.selectAll("circle");
+	circle
+		.transition()
+		.style("opacity", function(d) {
+			//console.log("Check: ", d.TIME_INDEX, ">", index)
+			if (d.TIME_INDEX == index) {
+				return 0.75;	
+			} else if(d.TIME_INDEX < index) {
+				return 0.2	
+			}
+			return 0.0;
+		})
+	//Update Web Text
+	var date = splitMonthYear(index);
+	d3.select("#date p#month").html(months[date[1]]);
+}
 
 /*
  *Generate Slider for animation
  */
 function generateSlider() {
-    var val = slider ? slider().value() : 0;
+	//sliderScale = d3.scale.linear().domain([0,orderedColumns.length - 1]);
+    sliderScale = getMaxTimeIndex(orderedColumns);
+	var val = slider ? slider().value() : 0;
     slider = d3.slider()
-		.scale(10);
-    //Associate slider with CSS classes
+		//.scale(sliderScale)
+		.axis(true)
+		.min(0)
+		.max(sliderScale)
+		.value(val)
+		.on("slide", function(event, value) {
+			currentFrame = value;
+			if (true) {
+				drawGlyphs(currentFrame)	
+			}
+		})
+		.axis(
+			//Create Slider Axis Titles
+			d3.svg.axis()
+				.scale(500)
+				.tickFormat(function(d) {
+					var date = splitMonthYear(d);
+					return months[date[0]] + " " + date[1];
+				})		
+		);
+ 
+	//Associate slider with CSS classes	
+	d3.select("#axis").remove();
     d3.select("#slider-dev").remove();
 	d3.select("#slider-container")
 		.append("div")
 		.attr("id", "slider-div")
+		.style("width", 500 + "px")
+		.on("mousemove", sliderBar)
+		.on("mouseout", function() {
+			d3.select("#slider-probe").style("display", "none");
+		})
 		.call(slider);
+
 }
-function splitMonthYear(line) {
-    var year = line.substring(0,4);
-    var month = line.substring(4);
+
+function getMaxTimeIndex(timeIndexColumns) {
+	var maxIndex = timeIndexColumns.length - 1;
+	return timeIndexColumns[maxIndex];	
+}
+
+/*
+ * Find the range of dates in the given dataset
+ */
+function createDataScale(range) {
+	return range.length - 1;
+}
+/*
+ * Generates the slider bar
+ */
+function sliderBar() {
+	var place = d3.mouse(this)[0];
+	d3.select("#slider-probe")
+		.style("left", d3.mouse(this)[0] + "px")
+		.style("display", "block")
+		.select("p");
+		//.html(months[place.getMonth()] + " " + place.getFullYear());
+}
+
+function splitMonthYear(index) {
+    var year = Math.floor(index / 12) + 50;
+	var month = index % 12;
     return[month, year];
 }
